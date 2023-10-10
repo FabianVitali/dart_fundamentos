fedc064f8f3b6e7fcdfc6ee11bf4f3f217eb964c
FabianVitali <fabianvitali@gmail.com>
2023-10-09 21:19:53 -0300

    Novo assunto adicionado 'Variáveis'
    Os seguintes tópicos foram abordados:
    - Tipos de variáveis
    - Nomeando corretamente variáveis
    - Tudo no Dart são objetos

---
+++ lib/1_variaveis/a_variaveis.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: unused_local_variable, non_constant_identifier_names
+
+void main() {
+  num qualquerNumero = 1.2;
+  num qualquernumero2 = 1.2;
+
+  // int idade = 37;
+  var idade = 37;
+
+  // double valor = 37.2;
+  var valor = 37.2;
+
+  var valorIphone = 12.000;
+  // Camel Case
+  var nomeCurso = 'Academia do FLutter';
+  String nomeCurso2 = 'Academia do FLutter';
+  String nomeMelhorCursoFlutter = 'Academia do Flutter';
+
+  // Snake Case
+  var nome_curso = 'Academia do Flutter';
+
+  String nomeCursoFlutter2;
+  nomeCursoFlutter2 = 'Academia do Flutter';
+
+  Object objetoQualquer = 1.2;
+  dynamic dynamicQualquer = 1;
+  dynamicQualquer = 'Fabian';
+  dynamicQualquer = 1.2;
+}
