b8c1692832c16424c3b76f718ca395bb762e94a6
Fabian Vitali <fabianvitali@MacBook-Air-de-Fabian.local>
2024-02-28 13:34:48 -0300

    Novo assunto adicionado 'variáveis'
    Os seguintes tópicos foram abordados:
    - Tipos de variáveis
    	int
    	double
    	String
    	var
    	num
    	Object
    	dynamic

---
+++ lib/a_variaveis/a_variaveis.dart
@@ -0,0 +1,27 @@
+void main() {
+  num qualquerNumero = 1.2;
+  num qualquerNumero2 = 1.2;
+
+  // int idade = 37;
+  var idade = 37;
+
+  // double valor = 37.2;
+  var valor = 37.2;
+
+  var valorIphone = 12.000;
+
+  // camelCase
+  String nomeCurso = 'Academia do Flutter';
+  var nomeMelhorCursoFlutter = 'Academia do Flutter';
+
+  // Snake case
+  var nome_curso = 'Academia do Flutter';
+
+  String nomeCursoFlutter2;
+  nomeCursoFlutter2 = 'Academia do Flutter';
+
+  Object objetoQualquer = 1.2;
+  dynamic dynamicQualquer = 1;
+  dynamicQualquer = 'Fabian';
+  dynamicQualquer = 1.2;
+}
